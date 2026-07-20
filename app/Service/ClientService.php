f9ce75c050e9147f999721e2db1d27a10ab148f2
AikyDJ <96826093+AikyDJ@users.noreply.github.com>
2026-07-20 11:49:12 +0300

    service init

---
+++ app/Service/ClientService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Service;
+
+use app\Models\Client;
+use app\Models\Operation;
+use app\Models\Typeoperation;
+use app\Models\Views\Operationclient;
+use app\Models\Views\Soldeclient;
+
+class ClientService{
+    private $clientModel;
+    private $operationModel;
+    private $typeOperationModel;
+    
+    // client detail get details
+    public function getClientDetails($code_client){
+
+    }
+
+}
